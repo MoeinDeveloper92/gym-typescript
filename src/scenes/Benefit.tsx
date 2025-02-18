bbaef889d114cf46c5cd24c1000586f8dc7585cc
Moein Samani Nejad <moeinsamani1992@gmail.com>
2025-02-18 11:14:07 +0100

    implement screen setup

--- src/scenes/Benefit.tsx
+++ src/scenes/Benefit.tsx
@@ -1,7 +1,7 @@
 import React, { JSX } from 'react';
 import { BenefitType } from '../shared/types';
 import { div } from 'framer-motion/client';
-
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Benefit = ({ benefit }: { benefit: BenefitType }) => {
   return (
     <div className="flex flex-col items-center gap-8 border p-5 mt-5 hover:-translate-y-3 transition-transform duration-300 rounded-lg">
@@ -12,9 +12,12 @@ const Benefit = ({ benefit }: { benefit: BenefitType }) => {
       </div>
       <p className="font-bold">{benefit.title}</p>
       <p className="text-center">{benefit.description}</p>
-      <button className="bg-primary-100 px-4 py-2 rounded text-black shadow-xl">
+      <AnchorLink
+        href="#contactus"
+        className="bg-primary-100 px-4 py-2 rounded text-black shadow-xl"
+      >
         readmore
-      </button>
+      </AnchorLink>
     </div>
   );
 };
